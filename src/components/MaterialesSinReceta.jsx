a32cc373e1ffb9b7df667f0efc90f93d6ac99149
htapia78 <htapia78@gmail.com>
2026-07-28 23:39:34 -0300

    Update MaterialesSinReceta with 87-item catalog from PDF

--- src/components/MaterialesSinReceta.jsx
+++ src/components/MaterialesSinReceta.jsx
@@ -1,94 +1,89 @@
 import React, { useState, useEffect } from 'react';
 
 const CATALOGO = [
-  { categoria: 'Canalización', material: 'Caja Octogonal Grande Metálica', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Caja Octogonal Chica Metálica', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Caja Rectangular Metálica', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Caja de Registro', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Caja Cuadrada 200x200 PVC', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Caja 10x10x8 c/tapa Metálica', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Caño PVC 20mm', unidad: 'mts' },
-  { categoria: 'Canalización', material: 'Caño PVC 25mm', unidad: 'mts' },
-  { categoria: 'Canalización', material: 'Caño PVC 40mm', unidad: 'mts' },
-  { categoria: 'Canalización', material: 'Conector 20mm', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Conector 25mm', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Conector 40mm', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Cupla Unión 20mm', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Cupla Unión 25mm', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Cupla Unión 40mm', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Curva 20mm', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Curva 25mm', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Curva 40mm', unidad: 'un' },
-  { categoria: 'Canaliz. Esp.', material: 'Caño PVC 32mm', unidad: 'mts' },
-  { categoria: 'Canaliz. Esp.', material: 'Curva PVC 32mm', unidad: 'un' },
-  { categoria: 'Canaliz. Esp.', material: 'Conector PVC 32mm', unidad: 'un' },
-  { categoria: 'Canaliz. Esp.', material: 'Unión PVC 32mm', unidad: 'un' },
-  { categoria: 'Cables', material: 'Cable Unipolar 1.5mm Rojo', unidad: 'mts' },
-  { categoria: 'Cables', material: 'Cable Unipolar 1.5mm Marrón', unidad: 'mts' },
-  { categoria: 'Cables', material: 'Cable Unipolar 1.5mm Negro', unidad: 'mts' },
-  { categoria: 'Cables', material: 'Cable Unipolar 1.5mm Celeste', unidad: 'mts' },
-  { categoria: 'Cables', material: 'Cable Unipolar 1.5mm V/A', unidad: 'mts' },
-  { categoria: 'Cables', material: 'Cable Unipolar 2.5mm Rojo', unidad: 'mts' },
-  { categoria: 'Cables', material: 'Cable Unipolar 2.5mm Marrón', unidad: 'mts' },
-  { categoria: 'Cables', material: 'Cable Unipolar 2.5mm Negro', unidad: 'mts' },
-  { categoria: 'Cables', material: 'Cable Unipolar 2.5mm Celeste', unidad: 'mts' },
-  { categoria: 'Cables', material: 'Cable Unipolar 2.5mm V/A', unidad: 'mts' },
-  { categoria: 'Cables Esp.', material: 'Cable Subterráneo 2x2.5+T 2.5mm', unidad: 'mts' },
-  { categoria: 'Cables Esp.', material: 'Cable Subterráneo 2x6mm', unidad: 'mts' },
-  { categoria: 'Cables Esp.', material: 'Cable Subterráneo 4x6mm', unidad: 'mts' },
-  { categoria: 'Cables Esp.', material: 'Cable Subterráneo 4x10mm', unidad: 'mts' },
-  { categoria: 'Cables Esp.', material: 'Cable Unipolar V/A 6mm', unidad: 'mts' },
-  { categoria: 'Cables Esp.', material: 'Cable Unipolar V/A 10mm', unidad: 'mts' },
-  { categoria: 'Cables Esp.', material: 'Cable Subterráneo 4x25mm', unidad: 'mts' },
-  { categoria: 'Cables Esp.', material: 'Cable Subterráneo 4x16mm', unidad: 'mts' },
-  { categoria: 'Cables Esp.', material: 'Cable Subterráneo 2x4mm', unidad: 'mts' },
-  { categoria: 'Cables Esp.', material: 'Cable Unipolar V/A 2.5mm', unidad: 'mts' },
-  { categoria: 'Ilum. + Tomas', material: 'Bastidor', unidad: 'un' },
-  { categoria: 'Ilum. + Tomas', material: 'Marco', unidad: 'un' },
-  { categoria: 'Ilum. + Tomas', material: 'Módulo Tapa Ciega', unidad: 'un' },
-  { categoria: 'Ilum. + Tomas', material: 'Punto (llave simple)', unidad: 'un' },
-  { categoria: 'Ilum. + Tomas', material: 'Toma Simple', unidad: 'un' },
-  { categoria: 'Ilum. + Tomas', material: 'Módulo TV', unidad: 'un' },
-  { categoria: 'Ilum. + Tomas', material: 'Módulo Teléfono', unidad: 'un' },
-  { categoria: 'Ilum. + Tomas', material: 'Llave combinada', unidad: 'un' },
-  { categoria: 'Ilum. + Tomas', material: 'Toma Encapsulado Ext.', unidad: 'un' },
-  { categoria: 'Ilum. + Tomas', material: 'Cartel de Salida', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Tablero 96 Polos', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Tablero 36 Polos', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Tablero 60 Polos', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Tablero 80 Polos', unidad: 'un' },
-  { categoria: 'Tableros', material: 'GABINETE METALICO IP65 750X900X210', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR TERMOMAGNETICO 2X10A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR TERMOMAGNETICO 2X16A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR TERMOMAGNETICO 4X40A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR TERMOMAGNETICO 4X20A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR TERMOMAGNETICO 4X25A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR TERMOMAGNETICO 4X32A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR TERMOMAGNETICO 4X63A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR DIFERENCIAL 2X40A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR DIFERENCIAL 4X25A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR DIFERENCIAL 4X40A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR TERMOMAGNETICO 1X10A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR TERMOMAGNÉTICO 4X80A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'INTERRUPTOR TERMOMAGNÉTICO 4X50A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Contactor 4P 32A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Contactor 12V 16A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Trafo 220V/12V 300W', unidad: 'un' },
-  { categoria: 'PAT', material: 'Jabalina 3/4" 1500mm', unidad: 'un' },
-  { categoria: 'PAT', material: 'Bloquete PAT 3/4" T1', unidad: 'un' },
-  { categoria: 'PAT', material: 'Cámara de PAT', unidad: 'un' },
-  { categoria: 'PAT', material: 'Carbonilla', unidad: 'un' },
-  { categoria: 'Canaliz. Esp.', material: 'Pegamento chico', unidad: 'un' },
-  { categoria: 'Canaliz. Esp.', material: 'Distribuidor Elent 4 7 125AP', unidad: 'un' },
-  { categoria: 'Canaliz. Esp.', material: 'Distribuidor Elent 4 12 125AP', unidad: 'un' },
-  { categoria: 'Canalización', material: 'Extractor Monofásico Interior', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Dimer Riel Din 10A', unidad: 'un' },
-  { categoria: 'Ilum. + Tomas', material: 'Cartel Salida', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Fusible 2A', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Voltímetro', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Amperímetro', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Piloto Rojo 220v', unidad: 'un' },
-  { categoria: 'Tableros', material: 'Bornerta Portafusible', unidad: 'un' },
+  { codigo: '00978', nombre: 'CAJA OCTOGONAL GRANDE METALICA', unidad: 'un', precio: 610.0 },
+  { codigo: '00976', nombre: 'CAJA OCTOGONAL CHICA METALICA', unidad: 'un', precio: 283.0 },
+  { codigo: '00988', nombre: 'CAJA RECTANGULAR METALICA', unidad: 'un', precio: 283.0 },
+  { codigo: '00918', nombre: 'CAJA CUADRADA 10X10 METALICA', unidad: 'un', precio: 758.0 },
+  { codigo: '102116', nombre: 'CAJA ESTANCA PLASTICA 210X210X110 BLANCA IP65', unidad: 'un', precio: 7516.0 },
+  { codigo: '102802', nombre: 'CAJA DE PASE 10X10X8', unidad: 'un', precio: 3340.0 },
+  { codigo: '104516', nombre: 'CAÑO PVC SEMI PESADO 20MM KALOP', unidad: 'mts', precio: 1529.0 },
+  { codigo: '104517', nombre: 'CAÑO PVC SEMI PESADO 25MM KALOP KL05003', unidad: 'mts', precio: 2158.0 },
+  { codigo: '01175', nombre: 'CAÑO PVC RIGIDO EXTRAPESADO 40MM GENROD', unidad: 'mts', precio: 5106.0 },
+  { codigo: '01535', nombre: 'CONECTOR PVC 20MM', unidad: 'un', precio: 128.0 },
+  { codigo: '01537', nombre: 'CONECTOR PVC 25MM', unidad: 'un', precio: 274.0 },
+  { codigo: '01539', nombre: 'CONECTOR PVC 40MM', unidad: 'un', precio: 794.0 },
+  { codigo: '06088', nombre: 'UNION PVC 20MM', unidad: 'un', precio: 108.0 },
+  { codigo: '06090', nombre: 'UNION PVC 25MM', unidad: 'un', precio: 167.0 },
+  { codigo: '06092', nombre: 'UNION PVC 40MM', unidad: 'un', precio: 610.0 },
+  { codigo: '01869', nombre: 'CURVA PVC 20MM', unidad: 'un', precio: 253.0 },
+  { codigo: '01873', nombre: 'CURVA PVC 25MM', unidad: 'un', precio: 483.0 },
+  { codigo: '01879', nombre: 'CURVA PVC 40MM', unidad: 'un', precio: 1563.0 },
+  { codigo: '01174', nombre: 'CAÑO PVC RIGIDO EXTRAPESADO 32MM GENROD', unidad: 'mts', precio: 4183.0 },
+  { codigo: '01876', nombre: 'CURVA PVC 32MM', unidad: 'un', precio: 919.0 },
+  { codigo: '01538', nombre: 'CONECTOR PVC 32MM', unidad: 'un', precio: 630.0 },
+  { codigo: '06091', nombre: 'UNION PVC 32MM', unidad: 'un', precio: 444.0 },
+  { codigo: '04371', nombre: 'ROLLO CABLE 1.5MM ROJO KALOP', unidad: 'mts', precio: 44262.0 },
+  { codigo: '04369', nombre: 'ROLLO CABLE 1.5MM MARRON KALOP', unidad: 'mts', precio: 44262.0 },
+  { codigo: '04370', nombre: 'ROLLO CABLE 1.5MM NEGRO KALOP', unidad: 'mts', precio: 44262.0 },
+  { codigo: '04368', nombre: 'ROLLO CABLE 1.5MM CELESTE KALOP', unidad: 'mts', precio: 44262.0 },
+  { codigo: '04372', nombre: 'ROLLO CABLE 1.5MM V/A KALOP', unidad: 'mts', precio: 44262.0 },
+  { codigo: '04383', nombre: 'ROLLO CABLE 2.5MM ROJO KALOP', unidad: 'mts', precio: 71343.0 },
+  { codigo: '04381', nombre: 'ROLLO CABLE 2.5MM MARRON KALOP', unidad: 'mts', precio: 71343.0 },
+  { codigo: '04382', nombre: 'ROLLO CABLE 2.5MM NEGRO KALOP', unidad: 'mts', precio: 71343.0 },
+  { codigo: '04380', nombre: 'ROLLO CABLE 2.5MM CELESTE KALOP', unidad: 'mts', precio: 71343.0 },
+  { codigo: '04384', nombre: 'ROLLO CABLE 2.5MM V/A KALOP', unidad: 'mts', precio: 71343.0 },
+  { codigo: '00477', nombre: 'BASTIDOR 3 MODULOS UNICO KALOP', unidad: 'un', precio: 526.0 },
+  { codigo: '04820', nombre: 'TAPA RECTANGULAR BLANCA CIVIL KALOP', unidad: 'un', precio: 505.0 },
+  { codigo: '04866', nombre: 'TAPON CIEGO BLANCO KALOP', unidad: 'un', precio: 133.0 },
+  { codigo: '02931', nombre: 'INTERRUPTOR UNIPOLAR BLANCO KALOP', unidad: 'un', precio: 1324.0 },
+  { codigo: '05666', nombre: 'TOMA TV TERMINAL BLANCO KALOP', unidad: 'un', precio: 3484.0 },
+  { codigo: '05649', nombre: 'TOMA TEL RJ11 BLANCO KALOP', unidad: 'un', precio: 1985.0 },
+  { codigo: '02838', nombre: 'INTERRUPTOR COMBINACION BLANCO KALOP', unidad: 'un', precio: 2151.0 },
+  { codigo: '00914', nombre: 'CAJA CAPSULADA PARA BASE 16A KL48880', unidad: 'un', precio: 3102.0 },
+  { codigo: '04622', nombre: 'SOPORTE CON TOMA KD48251', unidad: 'un', precio: 1729.0 },
+  { codigo: '104931', nombre: 'SEÑALIZADOR LED SALIDA MACROLED', unidad: 'un', precio: 23274.0 },
+  { codigo: '02629', nombre: 'GABINETE ESTANCO DIN 96 M 600X750X100 FORLI 10096', unidad: 'un', precio: 232251.0 },
+  { codigo: '106459', nombre: 'GABINETE ESTANCO DIN 36 M 300X450X150 FORLI 15036', unidad: 'un', precio: 143857.0 },
+  { codigo: '103087', nombre: 'GABINETE ESTANCO DIN 60 M 450X450X100 FORLI 10060', unidad: 'un', precio: 139037.0 },
+  { codigo: '02635', nombre: 'GABINETE ESTANCO DIN 80 M 450X750X100 FORLI 10080', unidad: 'un', precio: 190117.0 },
+  { codigo: '103537', nombre: 'GABINETE CON DUCTO 750X900X225 160M FORLI DF759022', unidad: 'un', precio: 894902.0 },
+  { codigo: '103868', nombre: 'TERMICA EASY9 2X10A 4.5KA SCHNEIDER', unidad: 'un', precio: 13915.0 },
+  { codigo: '103909', nombre: 'TERMICA EASY9 2X16A 4.5KA SCHNEIDER', unidad: 'un', precio: 13915.0 },
+  { codigo: '103879', nombre: 'TERMICA EASY9 4X40A 4.5KA SCHNEIDER', unidad: 'un', precio: 38235.0 },
+  { codigo: '103931', nombre: 'TERMICA EASY9 4X20A 4.5KA SCHNEIDER', unidad: 'un', precio: 30474.0 },
+  { codigo: '103878', nombre: 'TERMICA EASY9 4X25A 4.5KA SCHNEIDER', unidad: 'un', precio: 30474.0 },
+  { codigo: '103869', nombre: 'TERMICA EASY9 4X32A 4.5KA SCHNEIDER', unidad: 'un', precio: 38235.0 },
+  { codigo: '103881', nombre: 'TERMICA EASY9 4X63A 4.5KA SCHNEIDER', unidad: 'un', precio: 64273.0 },
+  { codigo: '103883', nombre: 'DISYUNTOR DIFERENCIAL EASY9 2X40A 30MA SCHNEIDER', unidad: 'un', precio: 62352.0 },
+  { codigo: '103884', nombre: 'DISYUNTOR DIFERENCIAL EASY9 4X25A 30MA SCHNEIDER', unidad: 'un', precio: 86468.0 },
+  { codigo: '103885', nombre: 'DISYUNTOR DIFERENCIAL EASY9 4X40A 30MA SCHNEIDER', unidad: 'un', precio: 92326.0 },
+  { codigo: '103870', nombre: 'TERMICA EASY9 1X10A 4.5KA SCHNEIDER', unidad: 'un', precio: 6856.0 },
+  { codigo: '05000', nombre: 'TERMICA C120N 4X80A SCHNEIDER A9N18372', unidad: 'un', precio: 315198.0 },
+  { codigo: '103880', nombre: 'TERMICA EASY9 4X50A 4.5KA SCHNEIDER', unidad: 'un', precio: 64273.0 },
+  { codigo: '105055', nombre: 'CONTACTOR TRIPOLAR 32A 220VCA CHINT NXC-32 220', unidad: 'un', precio: 42777.0 },
+  { codigo: '02961', nombre: 'JABALINA IRAM 3/4 1500MM ARGENJAB', unidad: 'un', precio: 21258.0 },
+  { codigo: '03668', nombre: 'TOMACABLE NORMALIZADO T3 (3/4")', unidad: 'un', precio: 4724.0 },
+  { codigo: '01004', nombre: 'CAMARA DE INSPECCION PVC 15X15', unidad: 'un', precio: 2272.0 },
+  { codigo: '00512', nombre: 'BOLSA DE CARBONILLA 12 KG.', unidad: 'un', precio: 6000.0 },
+  { codigo: '06949', nombre: 'CABLE SUBTERRANEO 3X2.5MM', unidad: 'mts', precio: 2760.0 },
+  { codigo: '100791', nombre: 'CABLE SUBTERRANEO 2X6MM', unidad: 'mts', precio: 4249.0 },
+  { codigo: '100204', nombre: 'CABLE SUBTERRANEO 4X6MM', unidad: 'mts', precio: 7713.0 },
+  { codigo: '101226', nombre: 'CABLE SUBTERRANEO 4X10MM', unidad: 'mts', precio: 12344.0 },
+  { codigo: '04398', nombre: 'ROLLO CABLE 6MM V/A KALOP', unidad: 'mts', precio: 169931.0 },
+  { codigo: '00862', nombre: 'CABLE UNIPOLAR 10MM VERDE/AMARILLO', unidad: 'mts', precio: 2900.0 },
+  { codigo: '104547', nombre: 'ADHESIVO PARA CAÑOS VALINCO 60 CC', unidad: 'un', precio: 1614.0 },
+  { codigo: '00538', nombre: 'BORNERA DISTRIBUCION TETRAPOLAR 7 POLOS 125A ELENT', unidad: 'un', precio: 23063.0 },
+  { codigo: '00539', nombre: 'BORNERA DISTRIBUCION TETRAPOLAR 12 POLOS 125A ELENT', unidad: 'un', precio: 29383.0 },
+  { codigo: '102757', nombre: 'CABLE SUBTERRANEO 3X25+N', unidad: 'mts', precio: 27634.0 },
+  { codigo: '00799', nombre: 'CABLE SUBTERRANEO 4X16MM', unidad: 'mts', precio: 19376.0 },
+  { codigo: '100789', nombre: 'CABLE SUBTERRANEO 2X4MM', unidad: 'mts', precio: 2930.0 },
+  { codigo: '103758', nombre: 'TIMER DIGITAL PARA RIEL DIN 8 PROGRAMAS 16A', unidad: 'un', precio: 18110.0 },
+  { codigo: '104322', nombre: 'EXTRACTOR DE AIRE PARA BAÑO 4 BLANCO KUDU', unidad: 'un', precio: 17100.0 },
+  { codigo: '102814', nombre: 'CARTUCHO FUSIBLE 10X38 2A', unidad: 'un', precio: 1701.0 },
+  { codigo: '107002', nombre: 'MINI VOLTIMETRO + AMPERIMETRO DE PANEL ROJO', unidad: 'un', precio: 12258.0 },
+  { codigo: '03892', nombre: 'PILOTO LUMINOSO PLASTICO 22MM 230V ROJO', unidad: 'un', precio: 3330.0 },
+  { codigo: '04492', nombre: 'SECCIONADOR PORTAFUSIBLE UNIPOLAR 10X38', unidad: 'un', precio: 10449.0 },
 ];
 
 const UNIDADES = ['un', 'mts', 'kg', 'lt'];
@@ -114,13 +109,14 @@ export default function MaterialesSinReceta({ proyectoId }) {
 
   const sugerencias = busqueda.trim()
     ? CATALOGO.filter(m =>
-        m.material.toLowerCase().includes(busqueda.toLowerCase())
+        m.nombre.toLowerCase().includes(busqueda.toLowerCase()) ||
+        m.codigo.includes(busqueda)
       ).slice(0, 8)
     : [];
 
   const handleSeleccionar = (materialObj) => {
-    setBusqueda(materialObj.material);
-    setNuevaUnidad(materialObj.unidad); // Usar unidad del catálogo
+    setBusqueda(materialObj.nombre);
+    setNuevaUnidad(materialObj.unidad);
     setMostrarSugerencias(false);
   };
 
@@ -156,7 +152,7 @@ export default function MaterialesSinReceta({ proyectoId }) {
   return (
     <div style={{ background: 'white', padding: '32px', borderRadius: '12px', boxShadow: '0 4px 12px rgba(0,0,0,0.08)' }}>
       <h2 style={{ color: '#1c2d4f', marginTop: 0 }}>📦 Materiales sin Receta</h2>
-      <p style={{ color: '#666', marginBottom: '24px' }}>Cables, bandejas, cañería y otros que no tienen receta. Busca en el catálogo o agrega manualmente.</p>
+      <p style={{ color: '#666', marginBottom: '24px' }}>Selecciona del catálogo o agrega manualmente.</p>
 
       {/* FORMULARIO */}
       <div style={{ background: '#f9fafb', padding: '20px', borderRadius: '10px', marginBottom: '20px', border: '1px solid #e5e7eb', position: 'relative' }}>
@@ -171,7 +167,7 @@ export default function MaterialesSinReceta({ proyectoId }) {
                 setMostrarSugerencias(true);
               }}
               onFocus={() => setMostrarSugerencias(true)}
-              placeholder="Busca material (ej: Cable Subterráneo, PVC)"
+              placeholder="Busca por nombre o código"
               style={{
                 width: '100%',
                 padding: '10px',
@@ -212,8 +208,8 @@ export default function MaterialesSinReceta({ proyectoId }) {
                     onMouseEnter={(e) => e.currentTarget.style.background = '#f9fafb'}
                     onMouseLeave={(e) => e.currentTarget.style.background = 'white'}
                   >
-                    <strong>{mat.material}</strong>
-                    <span style={{ color: '#999', marginLeft: '10px', fontSize: '11px' }}>({mat.unidad})</span>
+                    <strong>{mat.nombre}</strong>
+                    <span style={{ color: '#999', marginLeft: '10px', fontSize: '11px' }}>({mat.codigo})</span>
                   </div>
                 ))}
               </div>
